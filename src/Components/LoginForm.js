b81723623404962506df23037fa4edcb702642cc
Frank Choongsaeng <frankchoongsaeng@gmail.com>
2020-10-09 13:07:19 +0000

    Little Changes

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -22,7 +22,7 @@ function Login(props) {
                         <div className="media">
                         <div className="media-left">
                             <figure className="image is-48x48">
-                            <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder" />
+                            <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder" /> 
                             </figure>
                         </div>
                         <div className="media-content">
